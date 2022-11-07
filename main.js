911a6e3f5bd423946f45ff7d4540564c85ed0051
garvsl <garvsehgal7@gmail.com>
2022-11-06 22:15:22 -0500

    complete the design for the calculator and button presses

--- main.js
+++ main.js
@@ -29,4 +29,41 @@ function operate(operator, a, b){
     
 }
 
+const buttons = document.querySelectorAll('.row div')
+const output = document.querySelector('.output');
+const result = document.querySelector('.result')
+
+let pressed;
+let operator;
+let temp;
+let answer;
+for(let i = 0; i < buttons.length; i++){
+    function buttonPress(e){
+        if(buttons[i].textContent == 'C'){
+            output.textContent = '0'
+            result.textContent = ''
+        }else{
+            output.textContent += buttons[i].textContent
+            if(/^[0-9]+$/.test(buttons[i].textContent)){
+                temp = pressed;
+                pressed = buttons[i].textContent 
+            }else{
+                operator = buttons[i].textContent 
+            }
+            
+        }
+        console.log(pressed + operator + temp)
+        console.log(pressed)
+        console.log(operator)
+        
+    
+        if(output.textContent.length > 15){
+            output.textContent = 'ERROR'
+        }
+
+
+    }
+    buttons[i].addEventListener('click', buttonPress);
+
+}
 //5x4
